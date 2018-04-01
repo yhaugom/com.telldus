4b56516324350440bcfc5b147952d443ac867277
Yngve Bøe Haugom <yngve_haugom@hotmail.com>
2018-04-01 16:00:47 +0200

    Can up and .gitignore entry
    - Added .gitignore entry for DS_Store files
    - Deleted generic comments from Athom

--- drivers/TZDW-100/device.js
+++ drivers/TZDW-100/device.js
@@ -2,12 +2,6 @@
 const Homey = require('homey');
 const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
-/**
- * It is possible to use default system capability handlers (see: lib/zwave/system/capabilities), by registering a
- * capability without an options object (see below). There are also various standard ZwaveDevice implementations (see:
- * lib/zwave), some of them use settings and flow cards (which are optional) and can be found in
- * lib/system/(flows|settings).json.
- */
 class TelldusDoorWindowSensor extends ZwaveDevice {
 	
 	async onMeshInit() {
@@ -27,21 +21,6 @@ class TelldusDoorWindowSensor extends ZwaveDevice {
 				getOnOnline: true, // use only for battery devices
 			}
 		});
-		
-		// register a report listener
-/*
-		this.registerReportListener('SWITCH_BINARY', 'SWITCH_BINARY_REPORT', ( rawReport, parsedReport ) => {
-			console.log('registerReportListener', rawReport, parsedReport);
-		});
-*/
-
-/*
-		// Set configuration value that is defined in manifest
-		await this.configurationSet({id: 'motion_threshold'}, 10);
-
-		// Or set configuration value that is not defined in manifest
-		await this.configurationSet({index: 1, size: 2}, 10);
-*/
 	}
 
 	
