80fd0f3634b753f1a6fc89dbc9499d6716dc9b5b
Xet Erixon <xet.erixon@gmail.com>
2018-06-13 22:09:49 +0200

    Measure kinda work

--- drivers/TZWP-102/device.js
+++ drivers/TZWP-102/device.js
@@ -7,17 +7,16 @@ class TelldusTZWP102 extends ZwaveDevice {
 	async onMeshInit() {
 		
 		// enable debugging
-		// this.enableDebug();
+		this.enableDebug();
 		
 		// print the node's info to the console
-		// this.printNode();
+		//this.printNode();
 
-		this.registerCapability('onoff', 'BASIC');
+		this.registerCapability('onoff','BASIC');
 		this.registerCapability('onoff','SWITCH_BINARY');
-		// this.registerCapability('meter_power','METER');
-		// this.registerCapability('measure_power','SENSOR_MULTILEVEL');
-		// this.registerCapabilityListener('onoff', this.onCapabilityOnoff.bind(this));
-		// this.registerReportListener('SWITCH_BINARY', 'SWITCH_BINARY_REPORT', ( rawReport, parsedReport ) => {
+		this.registerCapability('meter_power','METER');
+		this.registerCapability('measure_power','METER')
+		// this.registerReportListener('METER', 'METER_REPORT', ( rawReport, parsedReport ) => {
 		// 	console.log('registerReportListener', rawReport, parsedReport);
 		// });		
 	}
