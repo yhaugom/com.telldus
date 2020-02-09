8e158781b63c5c768d1f19332e9897063e23e681
Yngve Bøe Haugom <yngve_haugom@hotmail.com>
2020-02-09 12:57:03 +0100

    Added capabilities to power plug mini
    measure_current, measure_voltage

--- drivers/TZWP-102/device.js
+++ drivers/TZWP-102/device.js
@@ -3,31 +3,42 @@ const Homey = require('homey');
 const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
 class TelldusTZWP102 extends ZwaveDevice {
-	
+
 	async onMeshInit() {
 		this.registerCapability('onoff','BASIC');
 		this.registerCapability('onoff','SWITCH_BINARY');
 		this.registerCapability('meter_power','METER');
 		this.registerCapability('measure_power','METER');
-		
+		this.registerCapability('measure_current','METER');
+		this.registerCapability('measure_voltage','METER');
 
-        this.registerCapabilityListener('button.reset_meter', async () => {
-            
-            // Maintenance action button was pressed, return a promise 
-            return this.meterReset();
-        });
 
-     	this.resetMeterAction = new Homey.FlowCardAction('TZWP-102_reset_meter')
-            .register().registerRunListener((args, state) => {
-                return args.device.resetMeterRunListener(args, state);
-            });   
+    this.registerCapabilityListener('button.reset_meter', async () => {
+
+        // Maintenance action button was pressed, return a promise
+         return this.meterReset();
+    });
+
+   	this.resetMeterAction = new Homey.FlowCardAction('TZWP-102_reset_meter')
+          .register().registerRunListener((args, state) => {
+              return args.device.resetMeterRunListener(args, state);
+          });
 	}
-	
+
 	async resetMeterRunListener(args, state) {
 		if (this.node.CommandClass.COMMAND_CLASS_METER) {
 			return await this.node.CommandClass.COMMAND_CLASS_METER.METER_RESET({});
 		} return Promise.reject('unknown_error');
 	}
+
+	async resetMeterFlowListener(args) {
+		if (this.node &&
+			this.node.CommandClass.COMMAND_CLASS_METER) {
+			return await this.node.CommandClass.COMMAND_CLASS_METER.METER_RESET({});
+		}
+		return Promise.reject('This device does not support meter resets');
+	}
+
 
 }
 
