173316bc670efa6c5af6d763fbc94e6957767c3c
Yngve Bøe Haugom <yngve_haugom@hotmail.com>
2020-09-13 20:12:54 +0200

    Update app.js
    Updating to Homey apps sdk v3

--- app.js
+++ app.js
@@ -4,31 +4,30 @@ const Homey = require('homey');
 
 class TelldusApp extends Homey.App {
 
-	onInit() {
+	async onInit() {
+		let currentPowerCondition = this.homey.flow.getConditionCard('current_power');
 
-		let currentPowerCondition = new Homey.FlowCardCondition('current_power');
+
+
 		currentPowerCondition
-			.register()
-			.registerRunListener(( args, state ) => {
+			.registerRunListener(async( args, state ) => {
 				var power = args.plugin_switch_mini.getCapabilityValue('measure_power');
 				var margin = args.W * args.errormargin / 100;
 
 				if ((power >= args.W - margin) && (power < args.W + margin))
 				{
-					return Promise.resolve( true );
+					return true;
 				}
-
-				return Promise.resolve( false );
+				return false;
 			});
 
-			let resetMeterAction = new Homey.FlowCardAction('TZWP-102_reset_meter');
+			let resetMeterAction = this.homey.flow.getActionCard('TZWP-102_reset_meter');
 			resetMeterAction
-	    	.register()
-				.registerRunListener((args, state) => {
+				.registerRunListener(async(args, state) => {
 					if (this.node &&
 						this.node.CommandClass.COMMAND_CLASS_METER) {
 						this.log('callback METER_RESET triggered');
-						return Promise.resolve(this.node.CommandClass.COMMAND_CLASS_METER.METER_RESET({}));
+						return this.node.CommandClass.COMMAND_CLASS_METER.METER_RESET({});
 						}
 						return Promise.reject('unknown_error');
 	          });
